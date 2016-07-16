ce984eb3cd646516d50235197572b51e641a2860
crumudgin <zac.chu@gmail.com>
2016-07-16 16:33:03 -0400

    (incorrect) predictions are succesfully being made

---
+++ src/RNNNumpy.py
@@ -0,0 +1,39 @@
+import numpy as np
+class RNNNumpy:
+
+    def __init__(self, wordDim, hiddenDim=100, bpttTruncate=4):
+        self.wordDim = wordDim
+        self.hiddenDim = hiddenDim
+        self.bpttTruncate = bpttTruncate
+        self.U = np.random.uniform(-np.sqrt(1.0/wordDim), np.sqrt(1.0/wordDim), (hiddenDim,wordDim))
+        self.V = np.random.uniform(-np.sqrt(1.0/hiddenDim), np.sqrt(1.0/hiddenDim), (wordDim, hiddenDim))
+        self.W = np.random.uniform(-np.sqrt(1.0/hiddenDim), np.sqrt(1.0/hiddenDim), (hiddenDim, hiddenDim))
+
+    def forward_propagation(self, x):
+        # The total number of time steps
+        T = len(x)
+        # During forward propagation we save all hidden states in s because need them later.
+        # We add one additional element for the initial hidden, which we set to 0
+        s = np.zeros((T + 1, self.hiddenDim))
+        s[-1] = np.zeros(self.hiddenDim)
+        # The outputs at each time step. Again, we save them for later.
+        o = np.zeros((T, self.wordDim))
+        # For each time step...
+        for t in np.arange(T):
+            # Note that we are indxing U by x[t]. This is the same as multiplying U with a one-hot vector.
+            s[t] = np.tanh(self.U[:, x[t]] + self.W.dot(s[t - 1]))
+            dot = self.V.dot(s[t])
+            print(self.V.dot(s[t]))
+            o[t] = np.exp(dot - np.max(dot))
+        return [o, s]
+
+    # def softmax(x):
+    #     """Compute softmax values for each sets of scores in x."""
+    #     e_x = np.exp(x - np.max(x))
+    #     return e_x / e_x.sum()
+
+    def predict(self, x):
+        # Perform forward propagation and return index of the highest score
+        o, s = self.forward_propagation(x)
+        return np.argmax(o, axis=1)
+
--- src/main.py
+++ src/main.py
@@ -1,17 +1,48 @@
+import numpy as np
+from src import RNNNumpy
+
 class RNN:
     vocabulary = {}
-    vocabularySize = len(vocabulary)
+    vocabularySize = 0
+    charByChar = []
+    indexToChar = {}
+    charToIndex = dict()
+    sentences = []
 
     def tokenizeSource(self, file):
         contents = open(file, 'r')
         counter = 1
         for i in contents:
+            sentence = []
             for j in i:
                 if self.vocabulary.get(j, -1) == -1:
                     self.vocabulary[j] = counter
+                    self.indexToChar[counter] = j
                     counter += 1
+                self.charByChar.append(j)
+                # self.indexToChar.append(self.vocabulary.get(j))
+                if(j != '\n'):
+                    sentence.append(j)
+            self.sentences.append(sentence)
+        self.vocabularySize = len(self.vocabulary)
+        print(self.sentences)
+
+    def xTrain(self):
+        return np.asarray([[self.vocabulary[w] for w in sent[:-1]] for sent in self.sentences])
+
+    def yTrain(self):
+        return np.asarray([[self.vocabulary[w] for w in sent[1:]] for sent in self.sentences])
 
 
 r = RNN()
 r.tokenizeSource('works.txt')
-print(r.vocabulary)
+print(r.vocabularySize)
+print(r.charToIndex)
+# print(r.vocabulary.values().)
+print(r.xTrain()[0])
+print(r.yTrain()[0])
+n = RNNNumpy.RNNNumpy(r.vocabularySize)
+# print(n.U)
+# print(n.V)
+# print(n.W)
+print([r.indexToChar[i] for i in n.predict(r.xTrain()[0])])
